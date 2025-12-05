e75097de08da3e4296ec2769016255386343bb5c
Eliott Wanner <eliott.wanner@gmail.com>
2025-12-05 01:46:53 +0100

    exercices s'affichent en fonction du nombre de points

--- src/decathlon/js/script.js
+++ src/decathlon/js/script.js
@@ -20,4 +20,45 @@ document.querySelector('input[type="button"]').addEventListener("click", () => {
     points.pointsQCM += (qcm3 === "Oui") ? 20 : 5;
     points.pointsQCM += parseInt(qcm4 || 0) * 10; // frequency: 0→0pts, 1→10pts, 2→20pts, 3→30pts
     points.pointsQCM += parseInt(qcm5 || 0) * 5;  // objective: 1→5pts, 2→10pts, 3→15pts
+
+    if (points.pointsQCM <= 50){
+        const exos = document.createElement("div");
+        exos.innerHTML = `
+                    <h3>Circuit pour les amateurs ou les futurs machines</h3>
+
+                    <h4>Exercice 1 : Les pompes !</h4>
+
+                  <figure>
+                    <video controls muted>
+                    <source  src="media/pompes.webm" type="video/webm"/>
+                    </video>
+                  </figure>
+
+                   <h4>Exercice 2 : Les montées de genoux !</h4>
+
+                  <figure>
+                    <video controls muted>
+                    <source  src="media/pompes.webm" type="video/webm" />
+                    </video>
+                  </figure>
+
+                   <h4>Exercice 3 : Les montées de genoux !</h4>
+
+                  <figure>
+                    <video controls muted>
+                    <source  src="media/pompes.webm" type="video/webm" />
+                    </video>
+                  </figure>
+
+                    <h4>Exercice 4 : Les montées de genoux !</h4>
+
+                  <figure>
+                    <video controls muted>
+                    <source  src="media/pompes.webm" type="video/webm" />
+                    </video>
+                  </figure>
+            `;
+
+        document.getElementsByClassName("amateur")[0].appendChild(exos);
+    }
 });
